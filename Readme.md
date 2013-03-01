1c3f69e9bd630647b3ae07540ef9f80db7363be6
adamsanderson <netghost@gmail.com>
2013-02-28 22:22:50 -0800

    Initial commit, minimal functionality.

---
+++ Readme.md
@@ -0,0 +1,25 @@
+caret
+=====
+
+Listen to and manipulate the text caret.
+
+_in progress_
+
+Installation
+------------
+
+    $ component install adamsanderson/caret
+
+API
+---
+
+...
+
+License
+-------
+
+  MIT
+
+---
+
+Adam Sanderson, http://monkeyandcrow.com
---
+++ index.js
@@ -0,0 +1,46 @@
+var Emitter = require('emitter');
+var events = require('event');
+var ELEMENT_NODE = document.ELEMENT_NODE;
+
+module.exports = Caret;
+
+
+// var caret = new Caret(getElementById('content'))
+// var el = caret.parentElement();
+// var text = caret.precedingText();
+// var text = caret.followingText();
+
+function Caret(el) {
+  this.el = el || document;
+  this.bind(el);
+  
+  
+}
+
+Emitter(Caret.prototype);
+
+Caret.prototype.bind = function(el) {
+  events.bind(el, "keyup", checkCaretPosition);
+  events.bind(el, "mouseup", checkCaretPosition);
+
+  var caret = this;
+  function checkCaretPosition(){
+    // TODO: Only trigger events on real changes
+    caret.emit("change");
+  }
+};
+
+Caret.prototype.parentElement = function(){
+  if (document.getSelection){
+    selection = document.getSelection();
+    if (selection.rangeCount){
+      node = selection.getRangeAt(0).startContainer;
+    } else {
+      node = document.body;
+    }
+  } else {
+    node = document.selection.createRange().parentElement();
+  }
+  
+  return node.nodeType == ELEMENT_NODE ? node : node.parentElement;
+};
